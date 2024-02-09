b920d5c158703a0ac09a953054694c112f887c5c
Jean-Christophe <loiseau.jc@gmail.com>
2024-02-09 08:13:14 +0100

    Std working precision, abs and rel. tolerance

--- src/LightROM.f90
+++ src/LightROM.f90
@@ -1,10 +1,43 @@
 module LightROM
   implicit none
+  include "dtypes.h"
+
   private
 
-  public :: say_hello
+  !> Global variables.
+  public :: greetings, wp, atol, rtol
+
 contains
-  subroutine say_hello
-    print *, "Hello, LightROM!"
-  end subroutine say_hello
+
+  subroutine greetings()
+    write(*, *)
+    write(*, *)
+    write(*, *) "-------------------------------------------------"
+    write(*, *) "-------------------------------------------------"
+    write(*, *)
+
+    write(*, *) "      _     _       _     _  ______ ________  ___"
+    write(*, *) "     | |   (_)     | |   | | | ___ \  _  |  \/  |"
+    write(*, *) "     | |    _  __ _| |__ | |_| |_/ / | | | .  . |"
+    write(*, *) "     | |   | |/ _` | '_ \| __|    /| | | | |\/| |"
+    write(*, *) "     | |___| | (_| | | | | |_| |\ \\ \_/ / |  | |"
+    write(*, *) "     \_____/_|\__, |_| |_|\__\_| \_|\___/\_|  |_/"
+    write(*, *) "               __/ |"
+    write(*, *) "              |___/"
+    
+    write(*, *)
+    write(*, *) "Developped by: Jean-Christophe Loiseau & Simon Kern,"
+    write(*, *) "               Arts & Métiers Institute of Technology, 2024,"
+    write(*, *) "               jean-christophe.loiseau@ensam.eu"
+
+    write(*, *) "Version -- 0.1.0"
+    write(*, *) "License -- BSD 3-Clause"
+    write(*, *)
+
+    write(*, *) "-------------------------------------------------"
+    write(*, *) "-------------------------------------------------"
+    write(*, *)
+    write(*, *)
+  end subroutine greetings
+
 end module LightROM
---
+++ src/dtypes.h
@@ -0,0 +1,3 @@
+integer, parameter :: wp = selected_real_kin(15, 307)
+      real(kind=wp), parameter :: atol = epsilon(1.0_wp)
+      real(kind=wp), parameter :: rtol = sqrt(atol)
