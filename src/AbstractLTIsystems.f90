4e6db4600fd192b354f6cd2bfeff165b870e22ea
Jean-Christophe <loiseau.jc@gmail.com>
2024-02-09 15:24:09 +0100

    Update abstract lti types

--- src/AbstractLTIsystems.f90
+++ src/AbstractLTIsystems.f90
@@ -1,5 +1,6 @@
 module lightrom_AbstractLTIsystems
-  use LightKrylov
+  !> Use the abstract linear operator types defined in LightKrylov.
+  use LightKrylov, only : abstract_linop
   implicit none
   include "dtypes.h"
 
@@ -15,11 +16,34 @@ module lightrom_AbstractLTIsystems
 
   !> Abstract continuous LTI system.
   type, extends(abstract_dynamical_system), abstract, public :: abstract_lti_system
+     !> Dynamics matrix.
+     class(abstract_linop), allocatable :: A
+     !> Input-to-state matrix.
+     class(abstract_linop), allocatable :: B
+     !> State-to-output matrix.
+     class(abstract_linop), allocatable :: C
+     !> Feedthrough matrix.
+     real(kind=wp)        , allocatable :: D(:, :)
+     contains
+       private
   end type abstract_lti_system
 
   !> Abstract discrete LTI system.
   type, extends(abstract_dynamical_system), abstract, public :: abstract_dlti_system
+     !> Dynamic matrix.
+     class(abstract_linop), allocatable :: A
+     !> Input-to-state matrix.
+     class(abstract_linop), allocatable :: B
+     !> State-to-output matrix.
+     class(abstract_linop), allocatable :: C
+     !> Feedthrough matrix.
+     real(kind=wp)        , allocatable :: D(:, :)
+     !> Sampling period.
+     real(kind=wp)                      :: dt = 1.0_wp
+   contains
+     private
   end type abstract_dlti_system
 
 contains
+
 end module lightrom_AbstractLTIsystems
