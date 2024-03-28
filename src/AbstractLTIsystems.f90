f3cb8a1dce8e89bf160ca3f08b45021bd4821664
Simon Kern <skern@mech.kth.se>
2024-03-28 15:38:26 +0100

    Extension of the abstract_lti_system type

--- src/AbstractLTIsystems.f90
+++ src/AbstractLTIsystems.f90
@@ -1,48 +1,57 @@
 module lightrom_AbstractLTIsystems
-  !> Use the abstract linear operator types defined in LightKrylov.
-  use LightKrylov, only : abstract_linop
-  implicit none
-  include "dtypes.h"
+   !> Use the abstract linear operator types defined in LightKrylov.
+   use LightKrylov, only : abstract_linop
+   implicit none
+   include "dtypes.h"
 
-  private
+   private
 
-  !-------------------------------------------------------
-  !-----     ABSTRACT LTI SYSTEM TYPE DEFINITION     -----
-  !-------------------------------------------------------
+   !-------------------------------------------------------
+   !-----     ABSTRACT LTI SYSTEM TYPE DEFINITION     -----
+   !-------------------------------------------------------
 
-  !> General abstract type for general system.
-  type, abstract, public :: abstract_dynamical_system
-  end type abstract_dynamical_system
+   !> General abstract type for general system.
+   type, abstract, public :: abstract_dynamical_system
+   end type abstract_dynamical_system
 
-  !> Abstract continuous LTI system.
-  type, extends(abstract_dynamical_system), abstract, public :: abstract_lti_system
-     !> Dynamics matrix.
-     class(abstract_linop), allocatable :: A
-     !> Input-to-state matrix.
-     class(abstract_linop), allocatable :: B
-     !> State-to-output matrix.
-     class(abstract_linop), allocatable :: C
-     !> Feedthrough matrix.
-     real(kind=wp)        , allocatable :: D(:, :)
-     contains
-       private
-  end type abstract_lti_system
-
-  !> Abstract discrete LTI system.
-  type, extends(abstract_dynamical_system), abstract, public :: abstract_dlti_system
-     !> Dynamic matrix.
-     class(abstract_linop), allocatable :: A
-     !> Input-to-state matrix.
-     class(abstract_linop), allocatable :: B
-     !> State-to-output matrix.
-     class(abstract_linop), allocatable :: C
-     !> Feedthrough matrix.
-     real(kind=wp)        , allocatable :: D(:, :)
-     !> Sampling period.
-     real(kind=wp)                      :: dt = 1.0_wp
+   !> Abstract continuous LTI system.
+   type, extends(abstract_dynamical_system), abstract, public :: abstract_lti_system
+      !> Dynamics matrix.
+      class(abstract_linop), allocatable :: A
+      !> Input-to-state matrix.
+      class(abstract_linop), allocatable :: B
+      !> State-to-output matrix.
+      class(abstract_linop), allocatable :: C
+      !> Feedthrough matrix.
+      real(kind=wp)        , allocatable :: D(:, :)
    contains
-     private
-  end type abstract_dlti_system
+      private
+      procedure(abstract_apply), deferred, public :: apply_Q
+      procedure(abstract_apply), deferred, public :: apply_Rinv
+   end type abstract_lti_system
+
+   abstract interface
+      subroutine abstract_apply(state)
+         use lightkrylov_AbstractVector
+         class(abstract_vector), intent(inout) :: state
+      end subroutine abstract_apply
+   end interface
+
+   !> Abstract discrete LTI system.
+   type, extends(abstract_dynamical_system), abstract, public :: abstract_dlti_system
+      !> Dynamic matrix.
+      class(abstract_linop), allocatable :: A
+      !> Input-to-state matrix.
+      class(abstract_linop), allocatable :: B
+      !> State-to-output matrix.
+      class(abstract_linop), allocatable :: C
+      !> Feedthrough matrix.
+      real(kind=wp)        , allocatable :: D(:, :)
+      !> Sampling period.
+      real(kind=wp)                      :: dt = 1.0_wp
+   contains
+      private
+   end type abstract_dlti_system
 
 contains
 
