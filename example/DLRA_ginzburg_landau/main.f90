5024a63c7890f55ebef4bcb528559f6bad7ade89
Simon Kern <skern@mech.kth.se>
2024-04-23 18:06:08 +0200

    Added tests for controlability and observability Gramians.

--- example/DLRA_ginzburg_landau/main.f90
+++ example/DLRA_ginzburg_landau/main.f90
@@ -20,16 +20,16 @@ program demo
 
    ! DLRA
    integer, parameter :: rkmax = 14
-   integer, parameter :: rk_X0 = 10
+   integer, parameter :: rk_X0 = 14
    logical, parameter :: verb  = .true.
-   logical, parameter :: save  = .false.
+   logical, parameter :: save  = .true.
    character*128      :: oname
    ! rk_B & rk_C are set in ginzburg_landau_base.f90
 
    integer  :: nrk, ntau, rk,  torder
-   real(kind=wp) :: tau, Tend
+   real(kind=wp) :: tau, Tend, Ttot
    ! vector of dt values
-   real(kind=wp), allocatable :: dtv(:)
+   real(kind=wp), allocatable :: tauv(:)
    ! vector of rank values
    integer, allocatable :: rkv(:)
 
@@ -41,42 +41,29 @@ program demo
 
    ! LR representation
    type(LR_state)                            :: X
-   type(state_vector), allocatable           :: U(:)
-   real(kind=wp) , allocatable               :: S(:,:)
    
    ! Initial condition
-   real(kind=wp)                             :: U0(2*nx, rkmax)
+   real(kind=wp)                             :: U0_in(2*nx, rkmax)
    real(kind=wp)                             :: S0(rkmax,rkmax)
-   !real(kind=wp)                            :: X0(nx,nx)
-!   
-   !! OUTPUT
-   !real(kind=wp)                        :: U_out(N,rkmax)
-   !real(kind=wp)                        :: X_out(N,N)
-!
-   !!> Information flag.
+   type(state_vector), allocatable           :: U0(:)
+   
+   ! OUTPUT
+   real(kind=wp)                             :: U_out(2*nx,rkmax)
+   real(kind=wp)                             :: X_out(2*nx,2*nx)
+
+   ! Information flag.
    integer                                   :: info
-   integer                                   :: i, j, k, irep, nrep
+   integer                                   :: i, j, k, irep, nrep, nsteps
 !
-   !! PROBLEM DEFINITION
-   !real(kind=wp)  :: Adata(N,N)
-   !real(kind=wp)  :: Bdata(N,rkmax)
-   !real(kind=wp)  :: BBTdata(N,N)
-   !
-   !! LAPACK SOLUTION
-   !real(kind=wp)  :: Xref(N,N)
-   !! DSYTD2
-   !real(kind=wp)  :: Dm(N), work(N), wr(N), wi(N)
-   !real(kind=wp)  :: E(N-1), tw(N-1)
-   !real(kind=wp)  :: T(N,N), Q(N,N), Z(N,N), Vdata(N,N), Wdata(N,N), Ydata(N,N)
-   !real(kind=wp)  :: scale
-   !integer   :: isgn
-   !! SVD
+   ! SVD
    real(kind=wp)  :: U_svd(2*nx,2*nx)
    real(kind=wp)  :: S_svd(rkmax)
    real(kind=wp)  :: V_svd(rkmax,rkmax)
 !
    ! timer
    integer   :: clock_rate, clock_start, clock_stop
+
+   call system_clock(count_rate=clock_rate)
 
    !----------------------------------
    !-----     INITIALIZATION     -----
@@ -97,33 +84,111 @@ program demo
 
    ! Define initial condition
    if (verb) write(*,*) 'Define initial condition'
-   allocate(U(1:rk_X0))
-   call init_rand(U, .false.)
-   call get_state(U0(:,1:rk_X0), U)
+   allocate(U0(1:rk_X0))
+   call init_rand(U0, .false.)
+   call get_state(U0_in(:,1:rk_X0), U0)
    ! Compute SVD to get low-rank representation
-   call svd(U0(:,1:rk_X0), U_svd(:,1:2*nx), S_svd(1:rk_X0), V_svd(1:rk_X0,1:rk_X0))
+   call svd(U0_in(:,1:rk_X0), U_svd(:,1:2*nx), S_svd(1:rk_X0), V_svd(1:rk_X0,1:rk_X0))
    S0 = 0.0_wp
    do i = 1,rk_X0
       S0(i,i) = S_svd(i)
    end do
-   call set_state(U, U_svd(:,1:rk_X0))
+   call set_state(U0, U_svd(:,1:rk_X0))
 
-   ! Initialize low-rank representation with rank rk
-   if (verb) write(*,*) 'Initialize LR state'
-   rk = 6
-   X = LR_state()
-   call X%initialize_LR_state(U, S0, rk)
-
-   tau  = 0.05_wp
+   tau  = 0.1_wp
    Tend = 1.0_wp
+   nrep = 2
 
-   ! run integrator
-   if (verb) write(*,*) 'Integrate ... t0 =', 0.0_wp, ', dt = ', tau
-   call system_clock(count=clock_start)     ! Start Timer
-   call numerical_low_rank_splitting_lyapunov_integrator(X, LTI%A, LTI%B, Tend, tau, 1, info, &
-                                                       & exptA=exptA, iftrans=.false., ifverb=verb)
-   call system_clock(count=clock_stop)      ! Stop Timer
-   if (verb) write(*,*) 'done. Tend =', Tend
+   nrk  = 4; allocate(rkv(1:nrk));   rkv  = (/ 2, 6, 10, 14 /)
+   ntau = 3; allocate(tauv(1:ntau)); tauv = (/ 0.1, 0.05, 0.01 /)
+
+   write(*,*) '---------------------'
+   write(*,*) '   CONTROLABILITY'
+   write(*,*) '---------------------'
+   do torder = 1, 2
+      do i = 1, nrk
+         rk = rkv(i)
+         do j = 1, ntau
+            tau = tauv(j)
+            ! Initialize low-rank representation with rank rk
+            if (verb) write(*,*) 'Initialize LR state, rk =', rk
+            X = LR_state()
+            call X%initialize_LR_state(U0, S0, rk)
+            ! Reset time
+            Ttot = 0.0_wp
+            write(*,'(A11,A4,A4,A10,A6,A8,A16,A20)') 'DLRA:','  rk',' TO','dt','steps','Tend','|| X_DLRA ||_2', 'Elapsed time'
+            nsteps = nint(Tend/tau)
+            do irep = 1, nrep
+               ! run integrator
+               call system_clock(count=clock_start)     ! Start Timer
+               call numerical_low_rank_splitting_lyapunov_integrator(X, LTI%A, LTI%B, Tend, tau, torder, info, &
+                                                                  & exptA=exptA, iftrans=.false., ifverb=.false.)
+               call system_clock(count=clock_stop)      ! Stop Timer
+
+               ! Reconstruct solution
+               call get_state(U_out(:,1:rk), X%U)
+               X_out = matmul(U_out(:,1:rk), matmul(X%S, transpose(U_out(:,1:rk))))
+
+               Ttot = Ttot + Tend
+               write(*,'(I4," ",A8,I4," TO",I1,F10.6,I6,F8.4,E16.8,F18.4," s")') irep, 'X OUTPUT', &
+                                 & rk, torder, tau, nsteps, Ttot, &
+                                 & norm2(X_out), real(clock_stop-clock_start)/real(clock_rate)
+            end do
+            if (rk == 14 .and. j == 3) then
+               if (save) then
+                  write(oname,'("example/DLRA_ginzburg_landau/GL_Xdata_TO",I1,"_rk",I2.2,"_t",I1,".npy")') torder, rk, j
+                  call save_npy(oname, X_out)
+               end if
+            end if
+            deallocate(X%U)
+            deallocate(X%S)
+         end do
+      end do
+   end do
+
+   write(*,*) '--------------------'
+   write(*,*) '   OBSERVABILITY'
+   write(*,*) '--------------------'
+   do torder = 1, 2
+      do i = 1, nrk
+         rk = rkv(i)
+         do j = 1, ntau
+            tau = tauv(j)
+            ! Initialize low-rank representation with rank rk
+            if (verb) write(*,*) 'Initialize LR state, rk =', rk
+            X = LR_state()
+            call X%initialize_LR_state(U0, S0, rk)
+            ! Reset time
+            Ttot = 0.0_wp
+            write(*,'(A11,A4,A4,A10,A6,A8,A16,A20)') 'DLRA:','  rk',' TO','dt','steps','Tend','|| X_DLRA ||_2', 'Elapsed time'
+            nsteps = nint(Tend/tau)
+            do irep = 1, nrep
+               ! run integrator
+               call system_clock(count=clock_start)     ! Start Timer
+               call numerical_low_rank_splitting_lyapunov_integrator(X, LTI%A, LTI%CT, Tend, tau, torder, info, &
+                                                                  & exptA=exptA, iftrans=.true., ifverb=.false.)
+               call system_clock(count=clock_stop)      ! Stop Timer
+
+               ! Reconstruct solution
+               call get_state(U_out(:,1:rk), X%U)
+               X_out = matmul(U_out(:,1:rk), matmul(X%S, transpose(U_out(:,1:rk))))
+
+               Ttot = Ttot + Tend
+               write(*,'(I4," ",A8,I4," TO",I1,F10.6,I6,F8.4,E16.8,F18.4," s")') irep, 'Y OUTPUT', &
+                                 & rk, torder, tau, nsteps, Ttot, &
+                                 & norm2(X_out), real(clock_stop-clock_start)/real(clock_rate)
+            end do
+            if (rk == 14 .and. j == 3) then
+               if (save) then
+                  write(oname,'("example/DLRA_ginzburg_landau/GL_Ydata_TO",I1,"_rk",I2.2,"_t",I1,".npy")') torder, rk, j
+                  call save_npy(oname, X_out)
+               end if
+            end if
+            deallocate(X%U)
+            deallocate(X%S)
+         end do
+      end do
+   end do
 
    return
 end program demo
