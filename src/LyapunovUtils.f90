cc2c6370f2c4e428d7654e304513a08683e541c6
Simon Kern <skern@mech.kth.se>
2024-06-13 14:47:58 +0200

    Extensions and refactoring

--- src/LyapunovUtils.f90
+++ src/LyapunovUtils.f90
@@ -1,6 +1,7 @@
 module LightROM_LyapunovUtils
    use LightKrylov
    use LightKrylov, only: wp => dp
+   use LightKrylov_Constants
    use LightKrylov_AbstractVectors
    
    implicit none
@@ -10,12 +11,40 @@ module LightROM_LyapunovUtils
    public :: apply_outerprod
 
    interface apply_outerprod
-      module procedure apply_outerprod_rdp
+      module procedure apply_outerprod_vector_rdp
+      module procedure apply_outerprod_basis_rdp
    end interface
 
    contains
 
-      subroutine apply_outerprod_rdp(C,A,B)
+      subroutine apply_outerprod_vector_rdp(c,A,b)
+         !! Computes the matrix product \( \mathbf{c} = \mathbf{Q} \mathbf{b} \) where
+         !! \( \mathbf{Q} = \mathbf{A} \mathbf{A}^T \) is the outer product of \( \mathbf{A} \)
+         !! with itself with
+         !!
+         !! - \( \mathbf{C} \): `abstract vector`
+         !! - \( \mathbf{A} \): `abstract vector` type Krylov basis of size (n x m)
+         !! - \( \mathbf{B} \): `abstract vector`
+         !!
+         !! In order to avoid building \( \mathbf{Q} \) (n x n), we compute sequentially
+         !! \( \mathbf{c} = \mathbf{A} ( \mathbf{A}^T \mathbf{b} ) \)
+         class(abstract_vector_rdp), intent(out) :: c
+         class(abstract_vector_rdp), intent(in)  :: A(:)
+         class(abstract_vector_rdp), intent(in)  :: b
+         ! Intermediate basis
+         real(wp) :: wrk(size(A))
+
+         wrk = zero_rdp
+         call innerprod(wrk, A, b)
+         block
+            class(abstract_vector_rdp), allocatable :: xwrk
+            call linear_combination(xwrk, A, wrk)
+            call c%zero(); call c%add(xwrk)
+         end block
+         return
+      end subroutine apply_outerprod_vector_rdp
+
+      subroutine apply_outerprod_basis_rdp(C,A,B)
          !! Computes the matrix product \( \mathbf{C} = \mathbf{Q} \mathbf{B} \) where
          !! \( \mathbf{Q} = \mathbf{A} \mathbf{A}^T \) is the outer product of \( \mathbf{A} \)
          !! with itself with
@@ -30,9 +59,9 @@ module LightROM_LyapunovUtils
          class(abstract_vector_rdp), intent(in)  :: A(:)
          class(abstract_vector_rdp), intent(in)  :: B(:)
          ! Intermediate basis
-         real(wp),      allocatable :: wrk(:,:)
+         real(wp) :: wrk(size(A),size(B))
 
-         allocate(wrk(1:size(A),1:size(B)))
+         wrk = zero_rdp
          call innerprod(wrk, A, B)
          block
             class(abstract_vector_rdp), allocatable :: Xwrk(:)
@@ -40,6 +69,6 @@ module LightROM_LyapunovUtils
             call copy_basis(C, Xwrk)
          end block
          return
-      end subroutine apply_outerprod_rdp
+      end subroutine apply_outerprod_basis_rdp
 
 end module LightROM_LyapunovUtils
