972051afce25cc3d8cdbb4a769a3c26a6fb486be
Simon KERN <johann-simon.kern@ensam.eu>
2026-02-04 22:04:54 +0100

    cleanup and add saving options

--- example/DLRA_ginzburg_landau/ginzburg_landau_tests_lyapunov.f90
+++ example/DLRA_ginzburg_landau/ginzburg_landau_tests_lyapunov.f90
@@ -19,13 +19,9 @@ module Ginzburg_Landau_Tests_Lyapunov
    use Ginzburg_Landau_Utils
    implicit none
 
-   integer,       parameter :: rkmax = 80
-   integer,       parameter :: rk_X0_lyapunov = 10
-
-   private :: this_module
-   public  :: rkmax, rk_X0_lyapunov
-
-   character(len=*), parameter :: this_module = 'Ginzburg_Landau_Tests_Lyapunov'
+   character(len=*), parameter, private :: this_module = 'Ginzburg_Landau_Tests_Lyapunov'
+   ! reference solutiions using RK
+   character(len=128), parameter :: fname_RK_base = 'Xref_RK_'
 
 contains
 
@@ -35,7 +31,7 @@ contains
    !
    !-------------------------------------------------------------------------------------------
 
-   subroutine run_lyapunov_reference_RK(LTI, Xref, Xref_RK, U0, S0, Tend, nrep, iref, adjoint)
+   subroutine run_lyapunov_reference_RK(LTI, Xref, Xref_RK, U0, S0, Tend, nrep, iref, adjoint, home, if_save_output)
       ! LTI system
       type(lti_system),              intent(inout) :: LTI
       ! Reference solution (BS)
@@ -49,25 +45,28 @@ contains
       integer,                       intent(in)    :: nrep
       integer,                       intent(in)    :: iref
       logical,                       intent(in)    :: adjoint
+      character(len=128),            intent(in)    :: home
+      logical,                       intent(in)    :: if_save_output
 
       ! Internals
       type(rk_lyapunov),             allocatable   :: RK_propagator
       type(state_matrix)                           :: X_mat(2)
-      real(dp),                      allocatable   :: X_RK(:,:,:)
+      real(dp),                      allocatable   :: X_RK(:,:,:), U_load(:,:), meta(:)
       real(dp)                                     :: U0_mat(N,N)
       integer                                      :: irep
-      real(dp)                                     :: etime, Tstep
+      real(dp)                                     :: etime0, etime, Tstep
       ! OUTPUT
       real(dp)                                     :: X_out(N,N)
-      character(len=128)                           :: note
+      character(len=256)                           :: note, fbase
+      logical                                      :: exist_file
       ! timer
-      integer :: clock_rate, clock_start, clock_stop
+      integer                                      :: clock_rate, clock_start, clock_stop
 
       character(len=*), parameter :: case = 'RKLIB'
       character(len=*), parameter :: eq   = 'lyap'
 
       call system_clock(count_rate=clock_rate)
-
+      
       Tstep = Tend/nrep
       allocate(X_RK(N, N, nrep))
       ! initialize exponential propagator
@@ -75,29 +74,61 @@ contains
       ! Set initial condition for RK
       call reconstruct_solution(X_out, U0, S0)
       call set_state(X_mat(1:1), X_out, 'Set initial condition')
+
+      print *, ''
+      print *, '#######################################################'
+      print *, '#                                                     #'
+      print *, '#             Solution using Runge-Kutta              #'
+      print *, '#                                                     #'
+      print *, '#######################################################'
+      print *, ''
+      print '(2X,A,F6.2)', 'Tend = ', Tend
+      print '(2X,A,I3)', 'nrep = ', nrep
+      print '(2X,A,I3)', 'iref = ', iref
+      print '(2X,A,L3)', 'save = ', if_save_output
+      print *, ''
+
       call print_header(case, eq)
       write(*,'(I7,F10.4,3(1X,E18.6),F10.4," s",A)') 0, 0.0, norm2(X_out)/N, norm2(X_out - Xref)/N, &
                                  & norm2(CALE(X_out, adjoint))/N, 0.0, ''
 
-      do irep = 1, nrep
-         call system_clock(count=clock_start)     ! Start Timer
-         ! integrate
-         if (adjoint) then
-            call RK_propagator%rmatvec(X_mat(1), X_mat(2))
-         else
-            call RK_propagator%matvec(X_mat(1), X_mat(2))
-         end if
-         call system_clock(count=clock_stop)      ! Stop Timer
-         etime = real(clock_stop-clock_start)/real(clock_rate)
-         ! recover output
-         call get_state(X_RK(:,:,irep), X_mat(2:2), 'Extract RK solution')
-         ! replace input
-         call set_state(X_mat(1:1), X_RK(:,:,irep), 'Reset initial condition')
-         ! print information
-         write(note,*) merge('   < reference', '              ', irep == iref)
-         call print_rklib_output(eq, irep, Tstep, X_RK, Xref, etime, note, adjoint)
-      enddo
-      Xref_RK(:,:) = X_RK(:,:,iref)
+      fbase = make_filename_RK(home, fname_RK_base, eq, Tend)
+      exist_file = exist_RK_file(fbase)
+      if (exist_file) then
+         call load_npy(trim(fbase)//'_X.npy', U_load)
+         call load_npy(trim(fbase)//'_meta.npy', meta)
+         irep  = 1
+         X_RK(:,:,irep) = U_load
+         etime = meta(1)
+         note = '   < reference'
+         call print_rklib_output(eq, irep, Tend, X_RK, Xref, etime, note, adjoint)
+         Xref_RK(:,:) = X_RK(:,:,iref)
+      else
+         etime = 0.0_dp
+         do irep = 1, nrep
+            call system_clock(count=clock_start)     ! Start Timer
+            ! integrate
+            if (adjoint) then
+               call RK_propagator%rmatvec(X_mat(1), X_mat(2))
+            else
+               call RK_propagator%matvec(X_mat(1), X_mat(2))
+            end if
+            call system_clock(count=clock_stop)      ! Stop Timer
+            etime0 = real(clock_stop-clock_start)/real(clock_rate)
+            etime = etime + etime0
+            ! recover output
+            call get_state(X_RK(:,:,irep), X_mat(2:2), 'Extract RK solution')
+            ! replace input
+            call set_state(X_mat(1:1), X_RK(:,:,irep), 'Reset initial condition')
+            ! print information
+            write(note,*) merge('   < reference', '              ', irep == iref)
+            call print_rklib_output(eq, irep, Tstep, X_RK, Xref, etime0, note, adjoint)
+         enddo
+         Xref_RK(:,:) = X_RK(:,:,iref)
+         call get_metadata(meta, eq, 0, 0, 0.0_dp, 0, Tend, Xref_RK, Xref_RK, Xref_RK, etime, adjoint)
+      end if
+      ! save output
+      if (if_save_output .and. .not. exist_file) call save_RK_state_npy(fbase, Xref_RK, meta)
       print *, ''
       print '(A,F16.12)', '  |  X_RK  |/N = ', norm2(Xref_RK)/N
       print '(A,F16.12)', '  | res_RK |/N = ', norm2(CALE(Xref_RK, adjoint))/N
@@ -126,15 +157,14 @@ contains
       logical,                       intent(in)    :: adjoint
       character(len=128),            intent(in)    :: home
       logical,                       intent(in)    :: if_save_output
+
       ! IO
-      real(wp),                      allocatable   :: U_load(:,:)
-      logical :: exist_file
+      real(wp),                      allocatable   :: meta(:)
+      logical                                      :: exist_file
       
       ! Internals
       character(len=256)                           :: fbase
-      character(len=256)                           :: fname
       type(LR_state),                allocatable   :: X
-      type(state_matrix)                           :: X_mat(2)
       integer                                      :: info, i, j, k, rk, torder, irep, nrep, nsteps
       real(dp)                                     :: etime, tau, Tstep, Ttot
       ! OUTPUT
@@ -155,12 +185,24 @@ contains
       
       Tstep = one_rdp
 
-      write(note,*) merge('Yobs', 'Xctl', adjoint)
+      note = merge('Yobs', 'Xctl', adjoint)
 
       ! basic opts
       opts = dlra_opts(chktime=one_rdp, inc_tol=atol_dp, if_rank_adaptive=.false.)
 
       call system_clock(count_rate=clock_rate)
+
+      print *, ''
+      print *, '#######################################################'
+      print *, '#                                                     #'
+      print *, '#           Solution using fixed-rank DLRA            #'
+      print *, '#                                                     #'
+      print *, '#######################################################'
+      print *, ''
+      print '(2X,A6,*(I6,1X))',    padr('TO  =',6), TOv
+      print '(2X,A6,*(I6,1X))',    padr('rk  =',6), rkv
+      print '(2X,A6,*(ES6.0,1X))', padr('tau =',6), dtv
+      print *, ''
 
       call print_header(case, eq)
       X = LR_state()
@@ -176,22 +218,14 @@ contains
                opts%mode = torder
 
                fbase = make_filename(home, case, eq, note, rk, torder, tau, Tend)
-               fname = fbase(:index(fbase,'.npy')-1)//'_U.npy'
-               inquire(file=trim(fname), exist=exist_file)
+               exist_file = exist_X_file(fbase)
                if (exist_file) then
-                  ! U
-                  call load_npy(trim(fname), U_load)
-                  X%rk = size(U_load, 2)
-                  allocate(X%U(X%rk), source=U0(1))
-                  call set_state(X%U, U_load, 'load_from_file')
-                  ! S
-                  fname = fbase(:index(fbase,'.npy')-1)//'_S.npy'
-                  call load_npy(trim(fname), U_load)
-                  allocate(X%S(X%rk,X%rk), source=U_load)
+                  call load_X_from_file(X, meta, fbase, U0)
+                  etime = meta(1)
                else
+                  call reset_timers()
                   ! Initialize low-rank representation with rank rk
                   call X%initialize_LR_state(U0, S0, rk, rkmax, .false.)
-
                   ! run integrator
                   call system_clock(count=clock_start)     ! Start Timer
                   if (adjoint) then
@@ -203,16 +237,19 @@ contains
                   end if
                   call system_clock(count=clock_stop)      ! Stop Timer
                   etime = real(clock_stop-clock_start)/real(clock_rate)
-                  ! save output
-                  if (if_save_output) then
-                     fname = make_filename(home, case, eq, note, rk, torder, tau, Tend)
-                     call save_LR_state_npy(fname, X, weight_mat)
-                  end if
                end if
                ! Reconstruct solution
                call reconstruct_solution(X_out, X)
+               ! Compute metadata
+               call get_metadata(meta, eq, rk, torder, tau, nsteps, tend, X_out, Xref_RK, Xref, etime, adjoint)
                ! print information
-               call print_dlra_output(eq, rk, torder, tau, nsteps, Tend, X_out, Xref_RK, Xref, etime, note, adjoint)
+               call print_dlra_output(eq, note, meta)
+               ! save output
+               if (if_save_output .and. .not. exist_file) then
+                  call save_LR_state_npy(fbase, X, weight_mat, meta)
+                  call save_metadata(fbase, case, eq, rk, torder, tau, nsteps, Tend, adjoint)
+               end if
+               ! cleanup
                deallocate(X%U); deallocate(X%S)
             end do
             print *, ''
@@ -249,15 +286,13 @@ contains
       character(len=128),            intent(in)    :: home
       logical,                       intent(in)    :: if_save_output
       ! IO
-      real(wp),                      allocatable   :: U_load(:,:)
+      real(wp),                      allocatable   :: meta(:)
       logical :: exist_file
 
       ! Internals
       character(len=256)                           :: fbase
-      character(len=256)                           :: fname
       type(LR_state),                allocatable   :: X
-      type(state_matrix)                           :: X_mat(2)
-      integer                                      :: info, i, j, k, rk, torder, irep, nsteps
+      integer                                      :: info, i, j, k, rk, torder, nsteps
       real(dp)                                     :: etime, tau
       ! OUTPUT
       real(dp)                                     :: X_out(N,N)
@@ -278,13 +313,25 @@ contains
       opts = dlra_opts(chktime=one_rdp, inc_tol=atol_dp, if_rank_adaptive=.true.)
 
       call system_clock(count_rate=clock_rate)
+      
+      note = merge('Yobs', 'Xctl', adjoint)
 
-      write(note,*) merge('Yobs', 'Xctl', adjoint)
+      print *, ''
+      print *, '#######################################################'
+      print *, '#                                                     #'
+      print *, '#         Solution using rank-adaptive DLRA           #'
+      print *, '#                                                     #'
+      print *, '#######################################################'
+      print *, ''
+      print '(2X,A6,*(I6,1X))',    padr('TO  =',6), TOv
+      print '(2X,A6,*(ES6.0,1X))', padr('tol =',6), tolv
+      print '(2X,A6,*(ES6.0,1X))', padr('tau =',6), dtv
+      print *, ''
  
-      call print_header(case, eq)
       rk = rk_X0_lyapunov
       X = LR_state()
       do i = 1, size(tolv)
+         call print_header(case, eq)
          opts%tol = tolv(i)
          print '(A,E9.2)', ' SVD tol = ', opts%tol
          print *, ''
@@ -297,26 +344,15 @@ contains
                ! set solver options
                opts%mode = torder
 
-               fbase = make_filename(home, case, eq, note, rk, torder, tau, Tend)
-               fname = fbase(:index(fbase,'.npy')-1)//'_U.npy'
-               inquire(file=trim(fname), exist=exist_file)
+               fbase = make_filename(home, case, eq, note, rk, torder, tau, Tend, opts%tol)
+               exist_file = exist_X_file(fbase)
                if (exist_file) then
-                  ! U
-                  call load_npy(trim(fname), U_load)
-                  X%rk = size(U_load, 2)
-                  allocate(X%U(X%rk), source=U0(1))
-                  call set_state(X%U, U_load, 'load_from_file')
-                  ! S
-                  fname = fbase(:index(fbase,'.npy')-1)//'_S.npy'
-                  call load_npy(trim(fname), U_load)
-                  allocate(X%S(X%rk,X%rk), source=U_load)
+                  call load_X_from_file(X, meta, fbase, U0)
+                  etime = meta(1)
                else
+                  call reset_timers()
                   ! Initialize low-rank representation with rank rk
                   call X%initialize_LR_state(U0, S0, rk, rkmax, opts%if_rank_adaptive)
-                  X%tot_time = 0.0_dp
-                  X%time     = 0.0_dp
-                  X%step     = 0
-
                   ! run integrator
                   call system_clock(count=clock_start)     ! Start Timer
                   if (adjoint) then
@@ -328,16 +364,19 @@ contains
                   end if
                   call system_clock(count=clock_stop)      ! Stop Timer
                   etime = real(clock_stop-clock_start)/real(clock_rate)
-                  ! save output
-                  if (if_save_output) then
-                     fname = make_filename(home, case, eq, note, rk, torder, tau, Tend, opts%tol)
-                     call save_LR_state_npy(fname, X, weight_mat)
-                  end if
                end if
                ! Reconstruct solution
                call reconstruct_solution(X_out, X)
+               ! Compute metadata
+               call get_metadata(meta, eq, X%rk, torder, tau, nsteps, tend, X_out, Xref_RK, Xref, etime, adjoint)
                ! print information
-               call print_dlra_output(eq, X%rk, torder, tau, nsteps, Tend, X_out, Xref_RK, Xref, etime, note, adjoint)
+               call print_dlra_output(eq, note, meta)
+               ! save output
+               if (if_save_output .and. .not. exist_file) then
+                  call save_LR_state_npy(fbase, X, weight_mat, meta)
+                  call save_metadata(fbase, case, eq, X%rk, torder, tau, nsteps, Tend, adjoint)
+               end if
+               ! cleanup
                deallocate(X%U); deallocate(X%S)
             end do
             print *, ''
