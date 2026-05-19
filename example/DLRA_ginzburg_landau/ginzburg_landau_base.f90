08d1d19e445ead622e590e9990e8429516c72220
Simon KERN <johann-simon.kern@ensam.eu>
2026-05-19 10:21:43 +0200

    typo in doc

--- example/DLRA_ginzburg_landau/ginzburg_landau_base.f90
+++ example/DLRA_ginzburg_landau/ginzburg_landau_base.f90
@@ -95,8 +95,8 @@ module Ginzburg_Landau_Base
    real(dp), parameter    :: x_b = -11.0_dp     ! location of input Gaussian
    real(dp), parameter    :: s_b = 1.0_dp       ! variance of input Gaussian
    type(state_vector)     :: B(rk_b)
-   real(dp), parameter    :: x_c = sqrt(-2.0_dp*(mu_0 - c_mu**2)/mu_2) ! location of input Gaussian
-   real(dp), parameter    :: s_c = 1.0_dp       ! variance of input Gaussian
+   real(dp), parameter    :: x_c = sqrt(-2.0_dp*(mu_0 - c_mu**2)/mu_2) ! location of output Gaussian
+   real(dp), parameter    :: s_c = 1.0_dp       ! variance of output Gaussian
    integer,  parameter    :: rk_c = 2           ! number of outputs to the system
    type(state_vector)     :: CT(rk_c)
    real(dp)               :: Qc(rk_c,rk_c)      ! State energy
